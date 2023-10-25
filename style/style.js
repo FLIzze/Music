1e4e016bd7857f185a59ffdb376b507866cc5590
alexandre <alexandre.bel@ynov.com>
2023-10-25 17:44:44 +0200

    first commit

---
+++ style/style.js
@@ -0,0 +1,10 @@
+window.addEventListener("mousemove", (e) => {
+    // cursor.style.top = e.y + "px";
+    // cursor.style.left = e.x + "px";
+
+    cursor2.style.top = e.pageY + "px";
+    cursor2.style.left = e.pageX + "px";
+
+    // cursor3.style.top = e.y + "px";
+    // cursor3.style.left = e.x + "px";
+});
